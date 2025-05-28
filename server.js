f55d9b9b26d6ee7754ef7bf282242f6c5e696d5f
NishithPrashanth <prashanthnishith@gmai.com>
2025-05-29 01:10:11 +0530

    Changes in the backend

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(cors({
     credentials: true, // Allows the use of cookies/credentials if required
   }));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
 });
